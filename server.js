ac7837c4dd70eec00d8a80cca37b4f9dfa818473
Mirza Tabish Hasan <73854161+Tabish8065@users.noreply.github.com>
2024-12-31 11:35:55 +0000

    kal

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const filePath = path.join(__dirname, 'out.txt'); // Path to the out.txt file
+
+app.post('/save-data', (req, res) => {
+    const { text } = req.body;
+
+    fs.appendFile(filePath, text + '\n', (err) => {
+        if (err) {
+            return res.status(500).send(err.toString());
+        }
+        res.status(200).send('');
+    });
+});
+
+app.get('/',(req, res) => {
+    console.log("Get");
+    res.status(200).send('done');
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
